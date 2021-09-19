458587586238e32d22057862958318ff6fdf5629
Jonathan Carnall <jonathancarnall@hotmail.com>
2021-09-19 20:33:16 +0100

    wip

--- notebooks/scratch/lsfr-scratch.fsx
+++ notebooks/scratch/lsfr-scratch.fsx
@@ -6,16 +6,34 @@
 // See the python script of same name, converted the python to f# for fun
 // https://www.youtube.com/watch?v=Ks1pw1X22y4
 
+// 0. Binary fundamentals
+
+let printBinaryn (binary: int) (n: int) = printfn "%s" (System.Convert.ToString(binary,2).PadLeft(n,'0'))
+printBinaryn 0b1001 4
+printBinaryn (0b1001 ||| 1) 4
+
+1 ||| 0
+1 ||| 1
+0 ||| 1
+0 ||| 0
+
+0 &&& 0
+0 &&& 1
+
+1 &&& 1
+0 &&& 1
+
+
 // 1. Simple 4 bit register
 
-// let printBinary (binary: int) = printfn "%s" (System.Convert.ToString(binary,2).PadLeft(4,'0'))
+let printBinary (binary: int) = printfn "%s" (System.Convert.ToString(binary,2).PadLeft(4,'0'))
 
-// let mutable state = 0b1001
-// for i = 1 to 20 do
-//     //printBinary state
-//     printf "%i" (state &&& 1)
-//     let newbit = (state ^^^ (state >>> 1)) &&& 1
-//     state <- (state >>> 1) ||| (newbit <<< 3)
+let mutable state = 0b1001
+for i = 1 to 20 do
+    //printBinary state
+    printf "%i" (state &&& 1)
+    let newbit = (state ^^^ (state >>> 1)) &&& 1
+    state <- (state >>> 1) ||| (newbit <<< 3) // or ing the left most *zero* bit to set it to the new bit
     
 
 // 2.Now do a 128 bit register 
@@ -26,12 +44,41 @@
 // If we use 'taps' 1,2 and 7 then we get the maxmium number of combinations, i.e 2 ^ 128 - 1, this can be mathematically
 // proven, will look into that another time?
 
-let mutable state = (1I <<< 127) ||| 1I // create 128 bits where first bit is one (non zero)
-while true do
-    printf "%s" ((state &&& 1I).ToString())    
+let mutable state = (1I <<< 127) ||| 1I // create 128 bits where first bit is one (non zero) and last bit is zero
+//while true do
+for i = 1 to 1000 do
+    printf "%s" ((state &&& 1I).ToString())  
+    //printfn "%s"  (state.ToString())
     let newbit = (state ^^^ (state >>> 1) ^^^ (state >>> 2) ^^^ (state >>> 7)) &&& 1I // use xor on 'taps' 1, 2 and 7 to get new bit
     state <- (state >>> 1) ||| (newbit <<< 127) // shift state right then set left most bit to the new bit
     
 
 // 3. Howabout a recursive function for the above for fun?
 
+// a. infinite
+let rec lsfr128 state: System.Numerics.BigInteger =
+    printf "%s" ((state &&& 1I).ToString())        
+    let newbit = (state ^^^ (state >>> 1) ^^^ (state >>> 2) ^^^ (state >>> 7)) &&& 1I
+    lsfr128 ((state >>> 1) ||| (newbit <<< 127))
+
+lsfr128 ((1I <<< 127) ||| 1I)
+
+//b. bounded
+
+let transform state: System.Numerics.BigInteger = 
+        let newbit = (state ^^^ (state >>> 1) ^^^ (state >>> 2) ^^^ (state >>> 7)) &&& 1I
+        ((state >>> 1) ||| (newbit <<< 127))
+
+let lsfrBounded (maxi: int) (initialState: System.Numerics.BigInteger) =    
+    let rec lsfrBounded' (state: System.Numerics.BigInteger) (i: int) = 
+        printf "%s" ((state &&& 1I).ToString())
+        match i with        
+        | i when i < maxi -> lsfrBounded' (transform state) (i + 1)
+        | _ -> ()
+    lsfrBounded' initialState 1
+
+
+lsfrBounded 10000 ((1I <<< 127) ||| 1I)
+
+        
+    
--- notebooks/scratch/lsfr-scratch.py
+++ notebooks/scratch/lsfr-scratch.py
@@ -5,25 +5,26 @@
 
 ## 1. let's do a simple 4 bit register first
 
-# state = 0b1001
+state = 0b1001
 
-# for i in range(20):
-#     # print("{:04b}".format(state))
-#     print(state & 1, end = '')
-#     # here we are xoring the right most bit with the bit to the left of it
-#     # we then 'and' with 1 to get the last bit
-#     newBit = (state ^ (state >> 1)) & 1
-#     # because when we shift our state to the right our left most bit becomes zero
-#     # if you 'or' a zero with something it will set that zero bit to whatever
-#     # the something is
-#     state = (state >> 1) | (newBit << 3) 
+for i in range(20):
+    # print("{:04b}".format(state))
+    print(state & 1, end = '')
+    # here we are xoring the right most bit with the bit to the left of it
+    # we then 'and' with 1 to get the last bit
+    newBit = (state ^ (state >> 1)) & 1
+    # because when we shift our state to the right our left most bit becomes zero
+    # if you 'or' a zero with something it will set that zero bit to whatever
+    # the something is
+    state = (state >> 1) | (newBit << 3) 
 
 #exit()   # if you want exit the repl 
 
 ## 2. Now do a 128 bit register!
 
 state = (1 << 127) | 1 # create 128 bits where first bit is one (non zero)
-while True:
+#while True:
+for i in range(1000):
     print(state & 1, end = '')
     # below we xor with 2nd, 3rd and 8th from right to get maximum length of cyle i.e 2^128 - 1
     newBit = (state ^ (state >> 1) ^ (state >> 2) ^ (state >> 7)) & 1
